aa3c22cda2bb291f3340620b4aa52030406f93e2
Matt <makeshifthidalgo@hotmail.com>
2018-07-31 16:53:10 -0700

    unable to read index.handlebars

--- burger/config/orm.js
+++ burger/config/orm.js
@@ -15,9 +15,17 @@ function updateOne() {
 };
 
 var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      }
 
 
-    
 }
 
 module.exports = orm;
--- burger/models/burger.js
+++ burger/models/burger.js
@@ -1,10 +1,14 @@
 var orm = require("../config/orm.js");
 
 var burger = {
+    all: function(cb) {
+        orm.all(" burgers", function(res) {
+          cb(res);
+        });
+      }
 
 
 
-    
 }
 
 
